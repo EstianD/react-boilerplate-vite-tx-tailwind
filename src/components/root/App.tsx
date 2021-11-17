26f49f40d736daccbf6823092ddee3919a9c3d5a
EstianD <code.estian@gmail.com>
2021-11-17 09:46:13 +0200

    Cleaned out some of stuff

--- src/components/root/App.tsx
+++ src/components/root/App.tsx
@@ -1,13 +1,8 @@
-import {HelmetProvider} from "react-helmet-async";
-import {AuthProvider} from "~/components/contexts/UserContext";
-import Main from "~/components/root/Main";
-
 export const App = () => {
   return (
-    <HelmetProvider>
-      <AuthProvider>
-        <Main />
-      </AuthProvider>
-    </HelmetProvider>
-  )
+    <div className="flex flex-col justify-center items-center h-screen bg-blue-200">
+      <h1 className="text-gray-200">Hello some text</h1>
+      <p className="text-gray-200">SOme more text</p>
+    </div>
+  );
 };
